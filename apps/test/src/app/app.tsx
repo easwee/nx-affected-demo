9a55857501b2a3c528661b91b6d578fc22a89df8
a.gorkic <anej.gorkic@global.id>
2022-01-06 10:21:20 +0100

    ci(actions): test INFRA-000

--- apps/test/src/app/app.tsx
+++ apps/test/src/app/app.tsx
@@ -1,3 +1,5 @@
+import { Dummy } from '@ttt/dummy';
+import { Dummy2 } from '@ttt/dummy-2';
 import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
 
@@ -9,6 +11,8 @@ export function App() {
   return (
     <StyledApp>
       <NxWelcome title="test" />
+      <Dummy />
+      <Dummy2 />
     </StyledApp>
   );
 }
